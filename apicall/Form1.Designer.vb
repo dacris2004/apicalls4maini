4e0d0cfa23985e8088b6d97278d4d6320f04b7d7
Daniel Cristea <daniel.cristea@ifacts.se>
2020-08-02 19:09:08 +0300

    Add project files.

---
+++ apicall/Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 30500
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(-1, 143)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(962, 225)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(21, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "..."
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 1000
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(830, 79)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(131, 32)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Reload settings"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Function"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 76)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Car"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 111)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(20, 20)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "S"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ToolStrip1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripLabel1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 371)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System
+        Me.ToolStrip1.Size = New System.Drawing.Size(962, 25)
+        Me.ToolStrip1.TabIndex = 6
+        Me.ToolStrip1.Text = "FTP"
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(52, 22)
+        Me.ToolStripLabel1.Text = "Status"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(962, 396)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "4maini - api call"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
+End Class
---
+++ apicall/Form1.vb
@@ -0,0 +1,314 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports ChoETL
+Imports Newtonsoft.Json
+Imports CsvHelper
+Imports System.Xml
+
+Public Class Form1
+    Dim sKey As String
+    Dim sSite As String
+    Dim sFunctions As String
+    Dim aFunctions() As String
+    Dim sCarsFile As String
+    Dim sFrom As String
+    Dim sTo As String
+    Dim sCar As String
+    Dim aCars() As String
+    Dim sInterval As String
+    Dim sFtp As String
+    Dim sUsername As String
+    Dim sPassword As String
+    Dim sFolder As String = "DATA"
+    Dim sDelete As String
+
+    Dim sDeleteFilesAfterUpload As Boolean = False
+    Dim sApiCall As String
+    Dim jsonRead As String
+    Dim countSeconds As Integer = 0
+    Dim carIndex As Integer = 0
+    Dim functionIndex As Integer = 0
+    Dim flagProcessingData As Boolean = False
+    Dim flagSendingData As Boolean = False
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Not My.Computer.FileSystem.FileExists("settings.ini") Then
+            If MsgBox("Settings file not found!!! Application could not run!", vbOKOnly, "4maini - api call") = vbOK Then
+                End
+            End If
+        Else
+
+            Dim My_Ini As New Ini("settings.ini")
+
+            'example of calling functions for ini file
+            'My_Ini.SetValue("Settings", "key", "230", True)
+            'My_Ini.DeleteValue("Settings", "site", True)
+            'My_Ini.DeleteSection("Settings", True)
+
+            sKey = My_Ini.GetValue("Settings", "key")
+            sSite = My_Ini.GetValue("Settings", "site")
+            sFunctions = My_Ini.GetValue("Settings", "functions")
+            aFunctions = Split(sFunctions, ",")
+
+            sCarsFile = My_Ini.GetValue("Settings", "cars")
+            sFrom = My_Ini.GetValue("Settings", "from")
+            sTo = My_Ini.GetValue("Settings", "to")
+            sInterval = My_Ini.GetValue("Settings", "interval")
+            sFtp = My_Ini.GetValue("Settings", "ftp")
+            sUsername = My_Ini.GetValue("Settings", "username")
+            sPassword = My_Ini.GetValue("Settings", "password")
+            sDelete = My_Ini.GetValue("Settings", "delete")
+
+            If sFtp = "" Or sUsername = "" Then
+                If MsgBox("ftp or/and username not defined in settings.ini file!!! Application could not run!", vbOKOnly, "4maini - api call") = vbOK Then
+                    End
+                End If
+            End If
+
+            If sKey = "" Then
+                sKey = "396b026cf8695cf23a63cad01ec712b7"
+            End If
+            If sSite = "" Then
+                sSite = "https://www.gpstracking.ro/api"
+            End If
+            If sFunctions = "" Then
+                sFunctions = "getQuestionnaires,getFilledQuestionnaires,getVehicleJourneys,getDailyActivity"
+            End If
+            If sCarsFile = "" Then
+                sCarsFile = "cars.txt"
+            End If
+            If sFrom = "" Then
+                sFrom = "2000-12-01"
+            End If
+            If sTo = "" Then
+                sTo = Format(Now, "yyyy-MM-dd")
+            End If
+            If sInterval = "" Then
+                sInterval = 30500
+            End If
+            If sDelete = "true" Or sDelete = "1" Then
+                sDeleteFilesAfterUpload = True
+            Else
+                sDeleteFilesAfterUpload = False
+            End If
+
+            aCars = File.ReadAllLines(sCarsFile)
+            carIndex = 0
+            functionIndex = 0
+
+            Debug.Print("key:" + sKey)
+            Debug.Print("site:" + sSite)
+            Debug.Print("functions:" + sFunctions)
+            Debug.Print("cars:" + sCarsFile)
+            Debug.Print("from:" + sFrom)
+            Debug.Print("to:" + sTo)
+            Debug.Print("aFunctions:" + aFunctions(0) + aFunctions(1) + aFunctions(2) + aFunctions(3))
+            Timer1.Interval = "1"
+            countSeconds = 0
+            Debug.Print("Creating data directory")
+            Try
+                My.Computer.FileSystem.CreateDirectory(sFolder)
+            Catch
+                If MsgBox("Error creating data folder! Application could not run!" + " Data folder: " + sFolder, vbOKOnly, "4maini - api call") = vbOK Then
+                    End
+                End If
+            End Try
+            Me.Text = Me.Text & " - Beta version"
+            If Now.Month > 9 Then
+                If MsgBox("Versiune beta! Contactati furnizorul pentru versiunea cu licenta!", vbOKOnly, "4maini - api call") = vbOK Then
+                    End
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If flagProcessingData Or flagSendingData Then
+            Exit Sub
+        End If
+        If carIndex < aCars.Length Then
+            sCar = aCars(carIndex).Replace(" ", "%20")
+            sApiCall = sSite + "?" + aFunctions(functionIndex) + "=" + sKey + "&cars[]=" + sCar + "&from=" + sFrom + "&to=" + sTo
+            Debug.Print("sApiCall:" + sApiCall)
+            'citeste json de la site
+            If ReadJson(sApiCall) Then
+                ListBox1.Items.Add(sApiCall)
+                ListBox1.SelectedIndex = ListBox1.Items.Count - 1
+                Label2.Text = aFunctions(functionIndex)
+                Label3.Text = aCars(carIndex)
+                carIndex = carIndex + 1
+            End If
+        Else
+            'reinitializeaza datele
+            carIndex = 0
+            functionIndex = functionIndex + 1
+            'tragem datele doar pentru 4 functii - de la capat
+            If functionIndex > 3 Then
+                functionIndex = 0
+                ListBox1.Items.Clear()
+                ' trimite datele - in caz ca se trimit la final de functie
+                ' ---------AICI----------
+            End If
+        End If
+        Timer1.Interval = CLng(sInterval)
+    End Sub
+    'afiseaza secundele pana la urmatoarea citire
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If flagProcessingData Or flagSendingData Then
+            Label1.Text = "..."
+            Exit Sub
+        End If
+        Dim value As Integer
+
+        countSeconds = countSeconds + 1
+        If countSeconds = CInt(Timer1.Interval / 1000) Then
+            countSeconds = 0
+        End If
+        value = CInt(Timer1.Interval / 1000) - countSeconds
+        If value < 0 Then
+            value = 0
+        End If
+        Label1.Text = value.ToString
+    End Sub
+
+
+    'reload settings
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form1_Load(sender, e)
+    End Sub
+
+    'functia apeleaza site-ul si salveaza datele primite
+    Private Function ReadJson(ByVal sUrl As String) As Boolean
+        Try
+            flagProcessingData = True
+            Dim request As WebRequest = WebRequest.Create(sUrl)
+            Dim response As WebResponse = request.GetResponse()
+            ' Display the status.  
+            Console.WriteLine(CType(response, HttpWebResponse).StatusDescription)
+            'show status
+            Label4.Text = CType(response, HttpWebResponse).StatusDescription
+            ' Get the stream containing content returned by the server.  
+            Dim dataStream As Stream = response.GetResponseStream()
+            ' Open the stream using a StreamReader for easy access.  
+            Dim reader As New StreamReader(dataStream)
+            ' Read the content.  
+            Dim responseFromServer As String = reader.ReadToEnd()
+            ' Display the content.  
+            'Console.WriteLine(responseFromServer)
+            ' Clean up the streams and the response.  
+            reader.Close()
+            response.Close()
+
+            'wait the right time for the request - by default, definded by site, 30 seconds
+            If responseFromServer.IndexOf("Too many requests") > 0 Then
+                flagProcessingData = True
+                ToolStripLabel1.Text = "Please wait 30 seconds! Too many api requests!"
+                If MsgBox(responseFromServer, vbOKOnly, "4maini - api call") = vbOK Then
+                    ToolStripLabel1.Text = "Please wait 30 seconds! Too many api requests!"
+                    Me.Refresh()
+                    Threading.Thread.Sleep(30500)
+                    Me.Button1.PerformClick()
+                    flagProcessingData = False
+                End If
+            End If
+            jsonRead = JsonConvert.DeserializeObject(responseFromServer).ToString()
+
+            'Console.WriteLine(jsonRead)
+            File.WriteAllText(sFolder + "/" + aFunctions(functionIndex) + "_" + carIndex.ToString + "_data.json", jsonRead)
+
+            'converteste datele pentru fiecare sofer
+            Dim r = New ChoJSONReader(sFolder + "/" + aFunctions(functionIndex) + "_" + carIndex.ToString + "_data.json")
+            Dim w = New ChoCSVWriter(sFolder + "/" + aFunctions(functionIndex) + "_" + carIndex.ToString + "_export.csv")
+            w.Write(r)
+
+            w.Close()
+            r.Close()
+
+            ' trimite datele ------------------------------------
+            If SendData(sFolder + "/" + aFunctions(functionIndex) + "_" + carIndex.ToString + "_export.csv", sFtp, sUsername, sPassword, sDeleteFilesAfterUpload) Then
+                Debug.Print("Data sent!")
+                ToolStripLabel1.ForeColor = Color.Black
+                ToolStripLabel1.Text = "Data sent!"
+            Else
+                Debug.Print("Data not sent!!! Error in sending data to the ftp server!")
+                ToolStripLabel1.ForeColor = Color.Red
+                ToolStripLabel1.Text = "Data not sent!!! Check your ftp server settings in the settings file! "
+            End If
+
+            flagProcessingData = False
+            Return True
+        Catch
+            Return False
+        End Try
+    End Function
+
+    Private Function SendData(ByVal fileToSend As String, ByVal ftpSite As String, ByVal user As String, ByVal pass As String, ByVal deleteAfterUpload As Boolean) As Boolean
+        flagSendingData = True
+
+
+        If UploadFTPFiles(ftpSite, user, pass, fileToSend, deleteAfterUpload) Then
+            flagSendingData = False
+            Return True
+        Else
+            flagSendingData = False
+            Return False
+        End If
+    End Function
+
+    Private Function UploadFTPFiles(ftpAddress As String, ftpUser As String, ftpPassword As String,
+                                   fileToUpload As String,
+                                   deleteAfterUpload As Boolean) As Boolean
+
+        Dim credential As NetworkCredential
+
+        Try
+            credential = New NetworkCredential(ftpUser, ftpPassword)
+
+            Dim sFtpFile As String = fileToUpload
+
+            Dim request As FtpWebRequest = DirectCast(WebRequest.Create("ftp://" & ftpAddress & "/" & Path.GetFileName(sFtpFile)), FtpWebRequest)
+
+            request.KeepAlive = False
+            request.Method = WebRequestMethods.Ftp.UploadFile
+            request.Credentials = credential
+            request.UsePassive = False
+            request.Timeout = (60 * 1000) * 3 '3 mins
+
+            Using reader As New FileStream(fileToUpload, FileMode.Open)
+
+                Dim buffer(Convert.ToInt32(reader.Length - 1)) As Byte
+                reader.Read(buffer, 0, buffer.Length)
+                reader.Close()
+
+                request.ContentLength = buffer.Length
+                Dim stream As Stream = request.GetRequestStream
+                stream.Write(buffer, 0, buffer.Length)
+                stream.Close()
+
+                Using response As FtpWebResponse = DirectCast(request.GetResponse, FtpWebResponse)
+
+                    If deleteAfterUpload Then
+                        My.Computer.FileSystem.DeleteFile(fileToUpload)
+                    End If
+
+                    response.Close()
+                End Using
+
+            End Using
+
+            Return True
+
+        Catch ex As Exception
+            'ExceptionInfo = ex
+            Return False
+        Finally
+
+        End Try
+
+    End Function
+End Class
